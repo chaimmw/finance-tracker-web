901803475b6b6cd077f0511e4470d231e9f8bac2
chaim <chaimmw86@gmail.com>
2018-08-30 16:45:53 -0400

    update nav bar

--- src/app/tracker/components/nav-bar/nav-bar.ts
+++ src/app/tracker/components/nav-bar/nav-bar.ts
@@ -1,5 +1,7 @@
 
 import {Component} from '@angular/core';
+import {FeathersClientService} from '../../services/feathers-client.service';
+import {Router} from '@angular/router';
 
 
 
@@ -10,7 +12,17 @@ import {Component} from '@angular/core';
 })
 export class NavBarComponent {
 
-    constructor() {
+    constructor(private router: Router,
+                private feathersClient: FeathersClientService) {
+    }
+
+    goToHome() {
+      this.router.navigate(['/tracker/dashboard']);
+    }
+
+    logout() {
+      this.feathersClient.logout();
+      this.router.navigate(['/login']);
     }
 
 }
